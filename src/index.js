6f5c01eda8cdb6b74a1c435e6d0acdf2f980730d
Matt Mink <mmink@oreillyauto.com>
2021-10-04 21:32:25 -0500

    testimonials carousel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-(() => {
-    const handleLinkClick = (e) => {
-        const { hash } = e.target;
+(($) => {
+    // Navigation
+    (function navigation() {
+        const handleLinkClick = (e) => {
+            const { hash } = e.target;
 
-        if (!hash) return;
+            if (!hash) return;
 
-        e.preventDefault();
+            e.preventDefault();
 
-        const el = document.querySelector(hash);
+            const el = document.querySelector(hash);
 
-        if (el) el.scrollIntoView({ behavior: 'smooth' });
-    }
+            if (el) el.scrollIntoView({ behavior: 'smooth' });
+        }
 
-    Array.from(document.querySelectorAll('a')).forEach((link) => {
-        link.addEventListener('click', handleLinkClick);
-    });
-})();
+        $('a').forEach((link) => {
+            link.addEventListener('click', handleLinkClick);
+        });
+    })();
+
+    // Testimonial Carousel
+    (function testimonials() {
+        const [prevBtn, nextBtn] = $('.testimonials-controls button');
+        const wrapper = document.querySelector('.testimonials-container');
+        const totalSlides = $('.testimonial').length;
+        let currentSlide = 1;
+
+        const goTo = (slideNumber) => {
+            if (slideNumber < 1 || slideNumber > totalSlides) return;
+
+            const newSlideIndex = slideNumber - 1;
+
+            wrapper.style.transform = `translateX(-${newSlideIndex * 100}vw)`;
+
+            currentSlide = slideNumber;
+
+            if (currentSlide === 1) {
+                prevBtn.disabled = true;
+                nextBtn.disabled = false;
+            } else if (currentSlide === totalSlides) {
+                prevBtn.disabled = false;
+                nextBtn.disabled = true;
+            } else {
+                prevBtn.disabled = false;
+                nextBtn.disabled = false;
+            }
+        };
+
+        prevBtn.addEventListener('click', () => goTo(currentSlide - 1))
+        nextBtn.addEventListener('click', () => goTo(currentSlide + 1));
+    })();
+})(selector => Array.from(document.querySelectorAll(selector)));
