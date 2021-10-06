b6af3dfa01ffb76d43e647bb01968d507b633032
Matt Mink <mmink@oreillyauto.com>
2021-10-06 07:14:57 -0500

    get contact info

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-(($) => {
+(() => {
+    const $ = selector => document.querySelector(selector);
+    const $$ = selector => Array.from(document.querySelectorAll(selector));
+
     // Navigation
     (function navigation() {
         const handleLinkClick = (e) => {
@@ -8,21 +11,21 @@
 
             e.preventDefault();
 
-            const el = document.querySelector(hash);
+            const el = $(hash);
 
             if (el) el.scrollIntoView({ behavior: 'smooth' });
         }
 
-        $('a').forEach((link) => {
+        $$('a').forEach((link) => {
             link.addEventListener('click', handleLinkClick);
         });
     })();
 
     // Testimonial Carousel
     (function testimonials() {
-        const [prevBtn, nextBtn] = $('.testimonials-controls button');
-        const wrapper = document.querySelector('.testimonials-container');
-        const totalSlides = $('.testimonial').length;
+        const [prevBtn, nextBtn] = $$('.testimonials-controls button');
+        const wrapper = $('.testimonials-container');
+        const totalSlides = $$('.testimonial').length;
         let currentSlide = 1;
 
         const goTo = (slideNumber) => {
@@ -49,4 +52,27 @@
         prevBtn.addEventListener('click', () => goTo(currentSlide - 1))
         nextBtn.addEventListener('click', () => goTo(currentSlide + 1));
     })();
-})(selector => Array.from(document.querySelectorAll(selector)));
+
+    // Get Contact Info
+    (function getContactInfo() {
+        const phoneForm = $('#phoneForm');
+        const phone = $('#phone');
+        const phoneParts = $$('#phone span');
+        const spamTest = $('#spamTest');
+
+        phoneForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            if (spamTest.value.trim().toLowerCase() !== 'mink') return;
+
+            String.fromCharCode(...'51565256495752534955'.match(/\d{1,2}/g).reverse())
+                .match(/(\d{3})(\d{3})(\d{4})/)
+                .slice(1)
+                .forEach((part, i) => {
+                    phoneParts[i].innerText = part;
+                });
+            phone.classList.remove('obfuscate');
+            phoneForm.remove();
+        });
+    })();
+})();
